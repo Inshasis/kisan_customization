bbb373887b166995b6495e3f73779464d8ad313e
inshasis <inshasistechnologies@gmail.com>
2026-08-30 20:12:01 +0530

    fix sales invoice

--- kisan_customization/sales_invoice/sales_order.py
+++ kisan_customization/sales_invoice/sales_order.py
@@ -6,10 +6,11 @@ from kisan_customization.payment_terms import apply_so_payment_terms_to_invoice,
 
 
 @frappe.whitelist()
-def make_sales_invoice(source_name, target_doc=None, ignore_permissions=False, args=None):
+def make_sales_invoice(source_name, target_doc=None, args=None):
+	"""Map Sales Order to Sales Invoice and apply custom payment/broker terms."""
 	from erpnext.selling.doctype.sales_order.sales_order import make_sales_invoice as erpnext_make_sales_invoice
 
-	doc = erpnext_make_sales_invoice(source_name, target_doc, ignore_permissions, args)
+	doc = erpnext_make_sales_invoice(source_name, target_doc=target_doc, args=args)
 	so = frappe.get_doc("Sales Order", source_name)
 	copy_broker_fields(so, doc)
 	apply_so_payment_terms_to_invoice(so, doc)
